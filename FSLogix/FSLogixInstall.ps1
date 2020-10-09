1e75c7efe05029e6c2a32fce3c207b097f071bbe
Luke Arp <ruarp@microsoft.com>
2020-10-09 14:06:03 -0400

    Working on DSC

--- FSLogix/FSLogixInstall.ps1
+++ FSLogix/FSLogixInstall.ps1
@@ -1,15 +1,13 @@
+param (
+    [string]$uncPath
+)
 
-function FSLogixInstall {
-    param (
-        [string]$uncPath
-    )
+Invoke-WebRequest -Uri https://aka.ms/fslogix_download -Method Get -OutFile fslogix.zip;
+Expand-Archive -Path fslogix.zip -Force;
+Set-Location .\fslogix\x64\Release;
+Start-Process -FilePath .\FSLogixAppsSetup.exe -ArgumentList "/install /quiet" -Verb runas -Wait;
+New-Item -Path HKLM:\SOFTWARE\FSLogix\Profiles
+New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name Enabled -PropertyType DWord -Value 1
+New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name DeleteLocalProfileWhenVHDShouldApply -PropertyType DWord -Value 1
+New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name VHDLocations -PropertyType MultiString -Value @($uncPath)    
 
-    Invoke-WebRequest -Uri https://aka.ms/fslogix_download -Method Get -OutFile fslogix.zip;
-    Expand-Archive -Path fslogix.zip -Force;
-    Set-Location .\fslogix\x64\Release;
-    Start-Process -FilePath .\FSLogixAppsSetup.exe -ArgumentList "/install /quiet" -Verb runas -Wait;
-    New-Item -Path HKLM:\SOFTWARE\FSLogix\Profiles
-    New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name Enabled -PropertyType DWord -Value 1
-    New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name DeleteLocalProfileWhenVHDShouldApply -PropertyType DWord -Value 1
-    New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name VHDLocations -PropertyType MultiString -Value @($uncPath)    
-}
